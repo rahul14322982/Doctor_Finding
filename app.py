0d50d33a54f093608fa7f9d7da32d34d396a120c
rahul k.s <rahulks14322@gmail.com>
2026-01-01 22:02:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -55,7 +55,8 @@ def predict_doctor():
         })
 
     except Exception as e:
-        return jsonify({"error": str(e)}), 500
+         port = int(os.environ.get("PORT", 5000))
+         app.run(host="0.0.0.0", port=port)
 
 
 if __name__ == "__main__":
